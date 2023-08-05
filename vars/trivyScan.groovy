69c70e5baa30207c77a63c5b86c9117d63e70859
nagesh <nageshle204@gmail.com>
2023-08-05 19:32:36 +0530

    modified mvnBuild.groovy

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -1,6 +1,7 @@
 def call ( String dockerHubuserName, String appname )
 {
-    sh """ trivy image ${dockerHubuserName}/${appname} ${dockerHubuserName}/${appname}:latest > scanreport.txt 
+    sh """ 
+    trivy image ${dockerHubuserName}/${appname}:latest > scanreport.txt 
     cat scanreport.txt
     """
 }
