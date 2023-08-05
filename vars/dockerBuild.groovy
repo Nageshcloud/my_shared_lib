206307b6aa494554037b8a632dafacdb5aa69558
nagesh <nageshle204@gmail.com>
2023-08-05 17:16:20 +0530

    modified mvnBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
 def call (String dockerHubuserName, String appname, String imageTag) {
-    docker build -t "${dockerHubuserName}"/"${appname}" .
-    docker image tag "${dockerHubuserName}"/"${appname}":"${imageTag}"
-    docker image tag "${dockerHubuserName}"/"${appname}":latest
+    docker build -t ${dockerHubuserName}/${appname} .
+    docker image tag ${dockerHubuserName}/${appname}:${imageTag}
+    docker image tag ${dockerHubuserName}/${appname}:latest
 }
